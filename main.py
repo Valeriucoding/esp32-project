22aec5fca309090ebd9eb1ac31bff12e0313e210
Valeriucoding <valeriucelmare1@gmail.com>
2024-07-31 23:57:35 +0300

    connecting more components to the project

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from machine import ADC, Pin
+from machine import ADC, Pin, PWM
 import time
 
+# Initialize potentiometer pin
 pot = ADC(Pin(26))
+# Initialize pir motion sensor pin
+pir_sensor = Pin(0, Pin.IN)
+# Initialize buzzer pin
+buzzer = PWM(Pin(1))
+buzzer.freq(1047)
+buzzer.duty_u16(0)
+# Initialize pushbutton pin
+button = Pin(11, Pin.IN, Pin.PULL_UP)
 
 while True:
-    pot_value = pot.read_u16() 
-    
+    # Potentiometer
+    pot_value = pot.read_u16()
+
     percentage = (pot_value / 65535) * 100
-    
+
     print("Potentiometer Value:", pot_value, "Percentage:", percentage)
-    
-    time.sleep(0.1) 
+
+    # Motion Sensor
+    motion_detected = pir_sensor.value()
+
+    if motion_detected:
+        print("Motion detected!")
+    else:
+        print("No motion")
+
+    # Buzzer
+    if motion_detected:
+        buzzer.freq(100)
+        buzzer.duty_u16(1)
+    else:
+        buzzer.duty_u16(0)
+
+    # Button
+    if button.value() == 0:
+        print("Button pressed!")
+    else:
+        print("Button not pressed")
+
+    time.sleep(0.1)
